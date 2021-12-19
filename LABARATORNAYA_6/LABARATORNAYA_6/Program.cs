68a04620d27d740f15778fed1d729f085326d359
JAKlup <90199477+JAKlup@users.noreply.github.com>
2021-12-19 19:48:50 +0300

    Add files via upload

---
+++ LABARATORNAYA_6/LABARATORNAYA_6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LABARATORNAYA_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[50];
+            Random r = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(0, 50);
+                Console.Write(" " + mas[i]);
+            }
+            Console.Read();
+            int maxMas = mas[0];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] > maxMas)
+                {
+                    maxMas = mas[i];
+                }
+            }
+            int maxCountValue = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (maxMas == mas[i])
+                {
+                    maxCountValue++;
+                }
+            }
+            Console.WriteLine("Максиальное значение " + maxMas);
+            Console.WriteLine("Максимальное значение повторяется " + maxCountValue + " раз");
+        }
+    }
+}
---
+++ LABARATORNAYA_6/Task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количесвто строк и столбцов квадратной матрицы");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] martix = new int[n, n];
+            int[,] martixATMD = new int[n, n];
+            int[,] matrixOTSD = new int[n, n];
+            Random rand = new Random();
+            Console.WriteLine("Исходная матрица:");
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    martix[i, j] = rand.Next(n * n);
+                    Console.Write(martix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Транспонирование матрицы по главной диагонали:");
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    martixATMD[i, j] = martix[j, i];
+                    Console.Write(martixATMD[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Транспонирование матрицы по побочной диагонали:");
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    matrixOTSD[i, j] = martix[(n - 1) - j, (n - 1) - i];
+                    Console.Write(matrixOTSD[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
